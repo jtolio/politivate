c7f6d62bbb9d071f1e64126ac6e6023bddf1844f
JT Olds <hello@jtolds.com>
2016-11-26 13:54:13 -0700

    changes

--- src/politiforce.jtolds.com/web/web.go
+++ src/politiforce.jtolds.com/web/web.go
@@ -2,10 +2,18 @@ package web
 
 import (
 	"net/http"
+
+	"github.com/jtolds/webhelp"
+	"golang.org/x/net/context"
 )
 
+func challenges(ctx context.Context, w webhelp.ResponseWriter, r *http.Request) error {
+	w.Write([]byte("hello 2"))
+	return nil
+}
+
 func init() {
-	http.HandleFunc("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello"))
-	}))
+	http.Handle("/", webhelp.Base{Root: webhelp.DirMux{
+		"challenges": webhelp.HandlerFunc(challenges),
+	}})
 }
