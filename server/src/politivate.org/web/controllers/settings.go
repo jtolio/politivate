d0cb65a73e171cf92a444b456af190c941bdc668
JT Olds <hello@jtolds.com>
2016-12-28 17:34:58 -0700

    get auth working

--- server/src/politivate.org/web/controllers/settings.go
+++ server/src/politivate.org/web/controllers/settings.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/jtolds/webhelp/whcompat"
 	"github.com/jtolds/webhelp/whmux"
 
 	"politivate.org/web/controllers/auth"
@@ -14,7 +15,9 @@ func init() {
 }
 
 func settings(w http.ResponseWriter, r *http.Request) {
+	ctx := whcompat.Context(r)
 	Render(w, r, "settings", map[string]interface{}{
 		"LogoutURL": auth.LogoutURL("/"),
+		"User":      auth.User(ctx),
 	})
 }
--- server/src/politivate.org/web/views/settings.go
+++ server/src/politivate.org/web/views/settings.go
@@ -4,6 +4,14 @@ var _ = T.MustParse(`{{ template "header" . }}
 
 <h1>Settings!</h1>
 
+<p><img src="{{.User.AvatarURL}}" /></p>
+
+<dl>
+<dt>Name</dt><dd>{{.User.Name}}</dd>
+<dt>NickName</dt><dd>{{.User.NickName}}</dd>
+<dt>Email</dt><dd>{{.User.Email}}</dd>
+</dl>
+
 <p><a href="{{.LogoutURL}}">Logout</a></p>
 
 {{ template "footer" . }}`)
