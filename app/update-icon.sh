8d99c25bf43f5e2709c9dca10edf062914f913b6
JT Olds <hello@jtolds.com>
2017-01-01 15:34:04 -0700

    slightly better app icon

---
+++ app/update-icon.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -f ../graphics/logos/plain/appicon48.png android/app/src/main/res/mipmap-mdpi/ic_launcher.png
+cp -f ../graphics/logos/plain/appicon72.png android/app/src/main/res/mipmap-hdpi/ic_launcher.png
+cp -f ../graphics/logos/plain/appicon96.png android/app/src/main/res/mipmap-xhdpi/ic_launcher.png
+cp -f ../graphics/logos/plain/appicon144.png android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
