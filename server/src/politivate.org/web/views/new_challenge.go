3fa94599ddfccafbc17ede7f65dfa51864177fd9
JT Olds <hello@jtolds.com>
2017-01-08 16:30:19 -0700

    new challenge outline

--- server/src/politivate.org/web/views/new_challenge.go
+++ server/src/politivate.org/web/views/new_challenge.go
@@ -2,4 +2,33 @@ package views
 
 var _ = T.MustParse(`{{ template "header" (makepair . "New Challenge") }}
 
+<p>User should enter:
+<ul>
+  <li>Title</li>
+  <li>Description</li>
+  <li>How many points</li>
+</ul></p>
+
+<p>User should pick:
+<ul>
+  <li>Phone call</li>
+  <li>Location</li>
+</ul></p>
+
+<p><ul>
+  <li>Enter a specific thing</li>
+  <li>Use a database</li>
+</ul></p>
+
+<p><ul>
+  <li>Deadline</li>
+  <li>Time range to post</li>
+</ul></p>
+
+<p>Restrict to<ul>
+  <li>States</li>
+  <li>Districts</li>
+  <li>Committees</li>
+</ul></p>
+
 {{ template "footer" . }}`)
