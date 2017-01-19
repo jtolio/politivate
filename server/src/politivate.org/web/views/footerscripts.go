4cd8bd1ed2257036e051cfbc822f5ab5a7f04b10
JT Olds <hello@jtolds.com>
2017-01-18 20:16:34 -0700

    include javascript

--- server/src/politivate.org/web/views/footerscripts.go
+++ server/src/politivate.org/web/views/footerscripts.go
@@ -2,6 +2,6 @@ package views
 
 var _ = T.MustParse(`
     </div>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="/static/js/jquery.min.js"></script>
+    <script src="/static/js/bootstrap.min.js"></script>
 `)
