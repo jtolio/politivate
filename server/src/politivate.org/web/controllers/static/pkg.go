b006adf3faa66d480b78b986da6311014a1c19d7
JT Olds <hello@jtolds.com>
2017-01-23 12:26:46 -0700

    move bootstrap to sass

--- server/src/politivate.org/web/controllers/static/pkg.go
+++ server/src/politivate.org/web/controllers/static/pkg.go
@@ -1,6 +1,7 @@
 package static
 
 //go:generate go install .../go-bindata...
+//go:generate bash -c "cd ../../sass && pyscss application.scss > ../static/css/bootstrap.css"
 //go:generate /usr/bin/env PATH=$GOPATH/bin go-bindata-assetfs -o bindata.go -pkg static --prefix ../../ ../../static/...
 
 import (
