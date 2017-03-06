453a197cf06a33c2ef15b51d8457e1139b098cb1
JT Olds <hello@jtolds.com>
2017-03-06 13:16:22 -0700

    stuff for pitch

--- server/src/politivate.org/web/controllers/pkg.go
+++ server/src/politivate.org/web/controllers/pkg.go
@@ -12,6 +12,8 @@ import (
 var (
 	mux = whmux.Dir{
 		"favicon.ico": whmux.Exact(whredir.RedirectHandler("/static/favicon.ico")),
+		"demo": whmux.Exact(whredir.RedirectHandler(
+			"https://appetize.io/app/exy765kug5pkf0qx118c0q9a4r")),
 	}
 
 	Handler http.Handler = mux
