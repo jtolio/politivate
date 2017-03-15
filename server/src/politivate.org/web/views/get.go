3fe67f5fb6b78d187b87d4db49a50f1ff44ad81c
JT Olds <hello@jtolds.com>
2017-03-15 16:34:55 -0600

    add demo app

--- server/src/politivate.org/web/views/get.go
+++ server/src/politivate.org/web/views/get.go
@@ -5,8 +5,9 @@ var _ = T.MustParse(`{{ template "header" (makepair . "Get the App!") }}
 <h1>Get the App!</h1>
 
 <p>The Politivate app will be coming soon to an iOS or Android app store near
-  you!</p>
+  you, but in the meantime, if you have Android, open the link below on
+  your phone to install the demo version!</p>
 
-<p><img src="/static/images/appicon144.png"></p>
+<p><a href="/static/demo.apk"><img src="/static/images/appicon144.png"></a></p>
 
 {{ template "footer" . }}`)
