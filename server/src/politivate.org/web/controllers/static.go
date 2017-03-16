6d70d6dfdf172c8fcf18cf8d51333a94fbd85adb
JT Olds <hello@jtolds.com>
2017-03-15 23:05:00 -0600

    whoops, fix bootstrap.css generation

--- server/src/politivate.org/web/controllers/static.go
+++ server/src/politivate.org/web/controllers/static.go
@@ -7,3 +7,7 @@ import (
 func init() {
 	mux["static"] = http.FileServer(http.Dir("./static/"))
 }
+
+//go:generate bash -c "cd ../sass && pyscss application.scss > ../static/css/bootstrap-new.css"
+//go:generate bash -c "cd ../static/css && if ! [ -e bootstrap.css ] || ! diff -q bootstrap.css bootstrap-new.css; then cp bootstrap{-new,}.css; fi"
+//go:generate rm -f ../static/css/bootstrap-new.css
