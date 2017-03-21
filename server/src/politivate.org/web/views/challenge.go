a6e2dd2a9b6dbb35af9c7057cff844d0764817ae
JT Olds <hello@jtolds.com>
2017-03-20 18:19:49 -0600

    clean up challenge delete button

--- server/src/politivate.org/web/views/challenge.go
+++ server/src/politivate.org/web/views/challenge.go
@@ -3,11 +3,14 @@ package views
 var _ = T.MustParse(`{{ template "header" (makepair . "Challenge") }}
 
 {{ if .Values.IsAdministrating }}
-  <ul>
-    <li><form method="post"><input type="hidden" name="action" value="delete"><input type="submit" value="Delete Challenge"></form></li>
-  </ul>
+  <!-- TODO: figure out how to get the btn-group to work with internal forms -->
+  <form method="post" onsubmit="return confirm('Are you sure you want to delete the challenge?');">
+    <input type="hidden" name="action" value="delete">
+    <div class="btn-group" role="group">
+      <button type="submit" class="btn btn-default">Delete Challenge</button>
+    </div>
+  </form>
 {{ end }}
-
 
 <h1>{{.Values.Challenge.Info.Title}}</h1>
 
