449fed8adb68c6529d084d3eb5e1d9cd11428a43
JT Olds <hello@jtolds.com>
2017-03-26 18:27:13 -0600

    legal pages

--- server/src/politivate.org/web/views/privacy.go
+++ server/src/politivate.org/web/views/privacy.go
@@ -1,9 +1,146 @@
 package views
 
-var _ = T.MustParse(`{{ template "header" (makemap "P" . "Title" "Privacy policy") }}
+var _ = T.MustParse(`{{ template "header" (makemap "P" . "Title" "Privacy Policy") }}
 
-<h1>Privacy policy</h1>
+<div class="row"><div class="col-sm-10 col-sm-offset-1">
 
-<p>Privacy policy coming soon!</p>
+<h1>Privacy Policy</h1>
+
+<p>Thank you for visiting Politivate (the "Site").
+Your privacy is important to us. Please review this Privacy Policy carefully.
+This Privacy Policy describes the policies and procedures of Politivate ("we",
+"our" or "us") on the collection, use, and disclosure of your information on
+the Site and the products, services, features, content, or applications we
+offer (the "Services"). When you use the Services, you are consenting to the
+collection, transfer, manipulation, storage, disclosure, and other uses of your
+information as described in this Privacy Policy.</p>
+
+<h3>What does this Privacy Policy cover?</h3>
+
+<p>This Privacy Policy covers the treatment of personally identifiable
+information ("Personal Information") gathered when you are using or accessing
+the Site and Services. This Privacy Policy does not apply to the practices of
+third parties that we do not own or control, including but not limited to
+Google, Facebook, or Twitter.</p>
+
+<h3>What Information Do We Collect?</h3>
+
+<p>We receive information about you from various sources, including the
+information you grant us via your login provider (Google, Facebook, Twitter),
+your Site and Service usage history, and any information you choose to provide
+us directly.</p>
+
+<p>Your personally identifiable information may be required to identify your
+user account, ascertain geographic distance to certain events and locations,
+and identify your elected representatives. Personally identifiable
+information may include but is not limited to: name, social media account ID,
+physical address, GPS location, and e-mail address. You acknowledge that this
+information may be personal to you, and by providing Personal Information to
+us, you allow others, including us, to identify you and therefore you may not
+be anonymous.</p>
+
+<h3>IP Address Information and Other Information Collected Automatically:</h3>
+
+<p>Finally, we may collect non-personally identifiable information when you
+visit our Site, such as your IP address.</p>
+
+<h3>IP Address Information:</h3>
+
+<p>While we collect and store IP address information, that information is not
+made public.</p>
+
+<h3>Information Collected Using Cookies:</h3>
+
+<p>Cookies are pieces of text that may be provided to your computer through
+your web browser when you access a website. We use cookies to enable our
+servers to recognize your web browser and tell us how and when you visit the
+Site and otherwise use the Services through the Internet. When logged in, our
+cookies also allow us to identify your user account and distinguish your
+actions from others.</p>
+
+<h3>Information You Elect to Share:</h3>
+
+<p>We are not responsible for the privacy policies and/or practices of these
+Third-Party Services, and you are responsible for reading and understanding
+those Third-Party Services' privacy policies. This Privacy Policy only governs
+information collected on the Services.</p>
+
+<h3>Aggregate Information:</h3>
+
+<p>We share Aggregate Information with Google and any other hosting providers.
+</p>
+
+<h3>Email Communications with Us:</h3>
+
+<p>As part of the Services, you may occasionally receive email and other
+communications from us, such as communications relating to your Account. We may
+use your contact information to send you these communications.</p>
+
+<h3>How Do We Use The Personal Information?</h3>
+
+<p>We may use the information you provide about yourself or others for purposes
+such as:</p>
+
+<ul>
+  <li>Identify who your elected representatives are.</li>
+  <li>Identify what events you may be able to attend.</li>
+  <li>Allow you to host a "profile" page detailing actions you've
+    completed using the Site and Services.</li>
+</ul>
+
+<p>Please note that if the Company is acquired, sold, or dissolved, its assets,
+including personally identifiable information we may possess about you, may, in
+whole or in part, be sold, disposed of, transferred, divested, or otherwise
+disclosed as part of the transaction or proceeding. You acknowledge that such
+transfers may occur and any acquirer may continue to use your Personal
+Information as set forth in this Privacy Policy.</p>
+
+<h3>Information Disclosed for Our Protection and the Protection of Others:</h3>
+
+<p>We also reserve the right to access, read, preserve, and disclose any
+information that we reasonably believe is necessary to (i) satisfy any
+applicable law, regulation, legal process, or governmental request, (ii)
+enforce this Privacy Policy, including investigation of potential violations
+hereof, (iii) detect, prevent, or otherwise address fraud, security, or
+technical issues, (iv) respond to user support requests, or (v) protect our
+rights, property, or safety, our users and the public. This includes exchanging
+information with other companies and organizations for fraud protection and
+spam/malware prevention.</p>
+
+<h3>Information We Share With Your Consent:</h3>
+
+<p>Except as set forth above, you will be notified when your Personal
+Information may be shared with third parties, and will be able to prevent the
+sharing of this information.</p>
+
+<h3>Is Information About Me Secure?</h3>
+
+<p>Although we take appropriate measures to safeguard against unauthorized
+disclosures of personally identifiable information, we cannot guarantee that
+personally identifiable information in our possession will never be disclosed
+in a manner that is inconsistent with this Privacy Policy. Unauthorized entry
+or use, hardware or software failure, and other factors may compromise the
+security of user information at any time. We otherwise store all of our
+information, including your IP address information, using industry-standard
+techniques. We do not guarantee or warrant that such techniques will prevent
+unauthorized access to information about you that we store, Personal
+Information or otherwise.</p>
+
+<h3>What Are My Privacy Options?</h3>
+
+<p>The Company will retain your Personal Information that we have collected as
+long we need it for purposes not prohibited by law.</p>
+
+<h3>Will We Update this Privacy Policy?</h3>
+
+<p>We may change this Privacy Policy at any time.</p>
+
+<h3>Contact Us.</h3>
+
+<p>If you have any questions or concerns about this Privacy Policy or its
+implementation please contact us at
+<a href="mailto:legal@politivate.org">legal@politivate.org</a>.</p>
+
+</div></div>
 
 {{ template "footer" . }}`)
--- server/src/politivate.org/web/views/tos.go
+++ server/src/politivate.org/web/views/tos.go
@@ -2,8 +2,278 @@ package views
 
 var _ = T.MustParse(`{{ template "header" (makemap "P" . "Title" "Terms of Service") }}
 
+<div class="row"><div class="col-sm-10 col-sm-offset-1">
+
 <h1>Terms of Service</h1>
 
-<p>Terms of service coming soon!</p>
+<p>These Terms of Service ("Terms") govern your access to and use of Politivate
+("Politivate", "we" or "our") websites, services, and applications
+(collectively the "Service"). Your access to and use of the Service is
+conditioned on your acceptance of and compliance with these Terms. These Terms
+apply to all visitors, users, and others who access or use the Service.</p>
+
+<h3>Your use of the service</h3>
+
+<p>By accessing or using the Service you agree to be bound by these Terms. If
+you are using the Services on behalf of an organization or entity
+("Organization"), then you are agreeing to these Terms on behalf of that
+Organization and you represent and warrant that you have the authority to bind
+the Organization to these Terms. In that case, "you" and "your" refers to you
+and that Organization.</p>
+
+<p>You may use the Service only if you can form a binding contract with
+Politivate, and only in compliance with these Terms and all applicable local,
+state, national, and international laws, rules, and regulations. The Service
+may change from time to time as we evolve, refine, or add more features to the
+Service, often without prior notice to you. In addition, Politivate may
+temporarily or permanently stop providing the Service, or any features within
+the Service, to you or to users generally and may not be able to provide you
+with prior notice.</p>
+
+<h3>Your content</h3>
+
+<p>Some areas of the Service allow you to post or upload information, text,
+graphics, or other material ("Content" or, when posted by you, "your Content"),
+and to share your Content with others. You retain ownership of your Content,
+but there are many things that users may do with your Content, for example,
+copy it, modify it, re-share it, or broadcast it. Politivate has no
+responsibility for that activity. You understand that publishing your Content
+on the Service is not a substitute for registering it with the U.S. Copyright
+Office, the Writers Guild of America, or any other rights organization.
+Please consider carefully what you choose to share.</p>
+
+<p>You agree not to post Content that (i) may create a risk of harm, loss,
+physical or mental injury, emotional distress, death, disability,
+disfigurement, or physical or mental illness to you, to any other person, or to
+any animal; (ii) may create a risk of any other loss or damage to any person or
+property; (iii) seeks to harm or exploit children by exposing them to
+inappropriate content, asking for personally identifiable details or otherwise;
+(iv) may constitute or contribute to a crime or tort; (v) contains any
+information or content that we deem to be unlawful, harmful, abusive, racially
+or ethnically offensive, defamatory, infringing, invasive of personal privacy
+or publicity rights, harassing, humiliating to other people (publicly or
+otherwise), libelous, threatening, profane, or otherwise objectionable; (vi)
+contains any information or content that is illegal (including, without
+limitation, the disclosure of insider information under securities law or of
+another party's trade secrets); (vii) contains any information or content that
+you do not have a right to make available under any law or under contractual or
+fiduciary relationships; or (viii) contains any information or content that you
+know is not correct and current. You agree that any Content that you post does
+not and will not violate rights of any kind, including without limitation any
+intellectual property rights or rights of privacy. We reserve the right` +
+
+	//`, but
+	//are not obligated, to reject and/or remove any Content that we believe, in our
+	//sole discretion, violates these provisions.`
+	` to remove any Content at any time.` +
+
+	`</p>
+
+<p>You retain full ownership to your Content, but you agree to grant us a
+worldwide, non-exclusive, irrevocable, transferable, perpetual, royalty-free
+license (with the right to sublicense) to use, copy, reproduce, process, adapt,
+modify, publish, transmit, edit, translate, make derivatives, display, and
+distribute such Content in connection with providing the Service to you and
+other users in accordance with your settings on the Service. In connection with
+providing the Service, we may modify or adapt your Content in order to
+transmit, display or distribute it over computer networks and in various media
+and/or make changes to your Content as are necessary to conform and adapt that
+Content to any requirements or limitations of any networks, devices, services,
+or media.</p>
+
+<p>We reserve the right at all times, but are not obligated, to remove or
+refuse to distribute any Content on the Service including your Content. We also
+reserve the right to access, read, preserve, and disclose any information as we
+reasonably believe is necessary to (i) satisfy any applicable law, regulation,
+legal process, or governmental request; (ii) enforce the Terms, including
+investigation of potential violations; (iii) detect, prevent, or otherwise
+address fraud, security, or technical issues; (iv) respond to user support
+requests; or (v) protect the rights, property, or safety of Politivate, its
+users, and the public.</p>
+
+<h3>Your account</h3>
+
+<p>You must provide us accurate information when you create your Politivate
+account. Your Politivate account gives you access to the Services and
+functionality that we may establish and maintain from time to time and in our
+sole discretion. We may maintain different types of accounts for different
+types of users or organizations. If you connect to Politivate with a
+third-party service, you give us permission to access and use your information
+from that service as permitted by that service. You may never use another
+user's account without permission.</p>
+
+<h3>Account security</h3>
+
+<p>You are responsible for safeguarding your credentials that you use to access
+the Service and for any activities or actions under your account, whether your
+account is with Politivate or a third-party service. We encourage you to use
+long passwords that are multiple unrelated words in length and not use the same
+password for different services. Politivate cannot and will not be liable for
+any loss or damage arising from your failure to comply with the above
+requirements. You must notify Politivate immediately upon becoming aware of any
+breach of security or unauthorized use of your account.</p>
+
+<h3>Politivate's license to you</h3>
+
+<p>Subject to these Terms, Politivate gives you a personal, worldwide,
+royalty-free, non-assignable, non-sublicensable, and non-exclusive license to
+use the Service. We reserve all rights not expressly granted in these Terms in
+the Service. We can terminate this license at any time for any reason or for no
+reason.</p>
+
+<p>We may make available software to access the Service via a mobile device
+("Mobile Software"). To use the Mobile Software you must have a mobile device
+that is compatible with the Mobile Service. We do not warrant that the Mobile
+Software will be compatible with your mobile device. We give you a
+non-exclusive, non-transferable, revocable license to use a compiled code copy
+of the Mobile Software for one account on one mobile device owned or leased
+solely by you, for your personal use. You may not (i) modify, disassemble,
+decompile, or reverse engineer the Mobile Software, except to the extent that
+such restriction is expressly prohibited by law; (ii) rent, lease, loan,
+resell, sublicense, distribute, or otherwise transfer the Mobile Software to
+any third party or use the Mobile Software to provide time sharing or similar
+services for any third party; (iii) make any copies of the Mobile Software;
+(iv) remove, circumvent, disable, damage, or otherwise interfere with
+security-related features of the Mobile Software, features that prevent or
+restrict use or copying of any content accessible through the Mobile Software,
+or features that enforce limitations on use of the Mobile Software; or (v)
+delete the copyright and other proprietary rights notices on the Mobile
+Software. You agree that we may from time to time issue upgraded versions of
+the Mobile Software, and may automatically electronically upgrade the version
+of the Mobile Software that you are using on your mobile device. You agree to
+the automatic upgrading on your mobile device, and agree that the terms and
+conditions of these Terms will apply to any upgrades. Any third-party code that
+may be incorporated in the Mobile Software is covered by the applicable open
+source or third-party license EULA, if any, authorizing use of such code. This
+license is not a sale of the Mobile Software or any copy thereof, and
+Politivate or its third-party partners or suppliers retain all right, title,
+and interest in the Mobile Software (and any copy thereof). We reserve all
+rights not expressly granted under these Terms. If the Mobile Software is being
+acquired on behalf of the United States Government, then the following
+provision applies: Use, duplication, or disclosure of the Mobile Software by
+the U.S. Government is subject to restrictions set forth in these Terms and as
+provided in DFARS 227.7202-1(a) and 227.7202-3(a) (1995), DFARS
+252.227-7013(c)(1)(ii) (OCT 1988), DFARS 252.227-7013(c) (2015), FAR 12.212(a)
+(1995), FAR 52.227-19, or FAR 52.227-14 (ALT III), as applicable. The Mobile
+Software originates in the United States, and is subject to United States
+export laws and regulations. The Mobile Software may not be exported or
+re-exported to certain countries or those persons or entities prohibited from
+receiving exports from the United States. In addition, the Mobile Software may
+be subject to the import and export laws of other countries. You agree to
+comply with all United States and foreign laws related to use of the Mobile
+Software and the Service.</p>
+
+<h3>Politivate property, copyrights, and feedback</h3>
+
+<p>All right, title, and interest in and to the Service (excluding Content
+provided by users) are and will remain the exclusive property of Politivate and
+its licensors. The Service is protected by copyright, trademark, and other laws
+of both the United States and foreign countries. Nothing in the Terms gives you
+a right to use the Politivate name or any of the Politivate trademarks, logos,
+domain names, and other distinctive brand features. Any feedback, comments, or
+suggestions you may provide regarding Politivate or the Service is entirely
+voluntary and we will be free to use such feedback, comments, or suggestions as
+we see fit and without any obligation to you.</p>
+
+<h3>Use of content</h3>
+
+<p>All Content in or on the Service, whether publicly posted or privately
+transmitted by users, is the sole responsibility of the person who originated
+such Content. We do not endorse, support, represent, or guarantee the
+completeness, truthfulness, accuracy, or reliability of any Content or
+communications posted via the Service or endorse any opinions expressed via the
+Service. We may not monitor or control the Content posted via the Service, and
+we cannot take responsibility for such Content. If you use or rely on any
+Content or materials posted via the Service or obtained by you through the
+Service, it is at your own risk. Under no circumstances will Politivate be
+liable in any way for any Content, or any loss or damage of any kind incurred
+as a result of the use of any Content in the Service.</p>
+
+<p>You agree that you are responsible for your use of the Service, for any
+Content you provide, and for any consequences thereof, including the use of
+your Content by other users and third parties. You understand that if you do
+not have the right to submit Content to the Service, doing so may subject you
+to liability. Politivate will not be responsible or liable for any use of your
+Content by Politivate in accordance with these Terms. You represent and warrant
+that you have all the rights, power, and authority necessary to grant the
+rights granted herein to any Content that you submit including all necessary
+rights to upload your Content for use in accordance with these terms and
+conditions.</p>
+
+<h3>Acceptable use of Politivate</h3>
+
+<p>Politivate is trusted by its users, and we trust you to use our Service
+responsibly. You agree not to misuse the Service. For example, you must not,
+and must not attempt to do the following things:</p>
+
+<ul>
+  <li>use the Service for any unlawful purposes or for promotion of illegal
+    activities;</li>
+  <li>post any Content on the Service in violation of any applicable law,
+    including intellectual property laws and right of privacy or publicity
+    laws, or any contractual obligation;</li>
+  <li>impersonate others through the Service or otherwise misrepresent your
+    affiliation with a person or entity in a manner that does or is intended to
+    mislead, confuse, or deceive others;</li>
+  <li>publish or post other people's private or personally identifiable
+    information, such as credit card numbers, street address or Social
+    Security/National Identity numbers, without their express authorization and
+    permission;</li>
+  <li>send unsolicited communications, promotions or advertisements, or
+    spam;</li>
+  <li>publish or link to malicious content intended to damage or disrupt
+    another user's browser or computer or to compromise a user's privacy;</li>
+  <li>access, tamper with, or use non-public areas of the Service, Politivate's
+    computer systems, or the technical delivery systems of Politivate's
+    providers;</li>
+  <li>probe, scan, or test the vulnerability of any system or network or breach
+    or circumvent any security or authentication measures;</li>
+  <li>forge any TCP/IP packet header or any part of the header information in
+    any email or posting, or in any way use the Service to send altered,
+    deceptive, or false source-identifying information;</li>
+  <li>interfere with, or disrupt, the access of any user, host or network,
+    including, without limitation, sending a virus, overloading, flooding,
+    spamming, or mail-bombing the Service, or by scripting the creation of
+    Content in such a manner as to interfere with or create an undue burden on
+    the Service. International users agree to comply with all local laws
+    regarding online conduct and acceptable content.</li>
+</ul>
+
+<p>We may investigate and/or suspend your account if you violate any of the
+above rules. Furthermore, we reserve the right to immediately terminate your
+account without further notice in the event that, in our sole and absolute
+judgment, you violate these Terms, or abuse the use of our Service.</p>
+
+<h3>Privacy</h3>
+
+<p>We care about the privacy of our users. We collect, use, and share
+personally identifiable information and non-personally identifiable information
+as described in our <a href="/legal/privacy">Privacy Policy</a>. You agree that
+by using the Service, you agree to the collection of such information, and to
+have your personal data collected, used, and transferred to and processed in
+the United States.</p>
+
+<h3>No warranty</h3>
+
+<p>THE SERVICE IS PROVIDED ON AN "AS IS" AND "AS AVAILABLE" BASIS. USE OF THE
+SERVICE IS AT YOUR OWN RISK. TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE LAW,
+THE SERVICE IS PROVIDED WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+IMPLIED, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. WITHOUT LIMITING THE
+FOREGOING, POLITIVATE AND ITS LICENSORS DO NOT WARRANT THAT THE CONTENT IS
+ACCURATE, RELIABLE OR CORRECT; THAT THE SERVICE WILL MEET YOUR REQUIREMENTS;
+THAT THE SERVICE WILL BE AVAILABLE AT ANY PARTICULAR TIME OR LOCATION,
+UNINTERRUPTED OR SECURE; THAT ANY DEFECTS OR ERRORS WILL BE CORRECTED; OR THAT
+THE SERVICE IS FREE OF VIRUSES OR OTHER HARMFUL COMPONENTS. ANY CONTENT
+DOWNLOADED OR OTHERWISE OBTAINED THROUGH THE USE OF THE SERVICE IS DOWNLOADED
+AT YOUR OWN RISK AND YOU WILL BE SOLELY RESPONSIBLE FOR ANY DAMAGE TO YOUR
+COMPUTER SYSTEM OR MOBILE DEVICE OR LOSS OF DATA THAT RESULTS FROM SUCH
+DOWNLOAD OR YOUR USE OF THE SERVICE. WE DO NOT WARRANT, ENDORSE, GUARANTEE, OR
+ASSUME RESPONSIBILITY FOR ANY PRODUCT OR SERVICE ADVERTISED OR OFFERED BY A
+THIRD PARTY THROUGH THE SERVICE OR ANY HYPERLINKED WEBSITE OR SERVICE, AND WE
+WILL NOT BE A PARTY TO OR IN ANY WAY MONITOR ANY TRANSACTION BETWEEN YOU AND
+THIRD-PARTY PROVIDERS OF PRODUCTS OR SERVICES.</p>
+
+</div></div>
 
 {{ template "footer" . }}`)
